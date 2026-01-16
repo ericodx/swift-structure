e150af75d222c2c02a8516dd90ba8ca147256af1
Érico Perez Neto <ericodx@icloud.com>
2026-01-16 20:27:07 -0300

    deps: add SwiftSyntax and ArgumentParser dependencies

--- Package.swift
+++ Package.swift
@@ -4,9 +4,18 @@ import PackageDescription
 let package = Package(
     name: "SwiftStructure",
     platforms: [.macOS(.v26)],
+    dependencies: [
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "602.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.7.0"),
+    ],
     targets: [
         .executableTarget(
-            name: "SwiftStructure"
+            name: "SwiftStructure",
+            dependencies: [
+                .product(name: "SwiftSyntax", package: "swift-syntax"),
+                .product(name: "SwiftParser", package: "swift-syntax"),
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ]
         ),
         .testTarget(
             name: "SwiftStructureTests",
