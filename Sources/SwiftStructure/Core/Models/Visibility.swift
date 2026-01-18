230b024d55f2608a57c46a2d9dcaae3ec75145a7
Érico Perez Neto <ericodx@icloud.com>
2026-01-18 09:00:34 -0300

    feat(models): add Visibility enum for member access levels

---
+++ Sources/SwiftStructure/Core/Models/Visibility.swift
@@ -0,0 +1,7 @@
+enum Visibility: String, Sendable, CaseIterable {
+    case `open` = "open"
+    case `public` = "public"
+    case `internal` = "internal"
+    case `fileprivate` = "fileprivate"
+    case `private` = "private"
+}
