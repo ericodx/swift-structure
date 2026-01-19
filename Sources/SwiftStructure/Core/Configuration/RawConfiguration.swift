fdfdea30e20c4e227f71aec3d20268375a001de2
Érico Perez Neto <ericodx@icloud.com>
2026-01-19 08:25:09 -0300

    feat: add annotated parameter to method rule in RawConfiguration

--- Sources/SwiftStructure/Core/Configuration/RawConfiguration.swift
+++ Sources/SwiftStructure/Core/Configuration/RawConfiguration.swift
@@ -8,5 +8,5 @@ struct RawConfiguration: Equatable, Sendable {
 enum RawMemberRule: Equatable, Sendable {
     case simple(String)
     case property(annotated: Bool?, visibility: String?)
-    case method(kind: String?, visibility: String?)
+    case method(kind: String?, visibility: String?, annotated: Bool?)
 }
