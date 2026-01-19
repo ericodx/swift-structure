777d6a01230c69f3a88e6361609a7034fa77b3bb
Érico Perez Neto <ericodx@icloud.com>
2026-01-19 13:24:25 -0300

    feat(errors): add LocalizedError to FileWritingError

--- Sources/SwiftStructure/Infrastructure/Files/FileWritingError.swift
+++ Sources/SwiftStructure/Infrastructure/Files/FileWritingError.swift
@@ -1,5 +1,12 @@
 import Foundation
 
-enum FileWritingError: Error {
+enum FileWritingError: Error, LocalizedError {
     case writeError(String, Error)
+
+    var errorDescription: String? {
+        switch self {
+        case .writeError(let path, let underlyingError):
+            return "Failed to write '\(path)': \(underlyingError.localizedDescription)"
+        }
+    }
 }
