7fc02238ecae66cc2675a68a73ffbd1ee6f9f69e
Érico Perez Neto <ericodx@icloud.com>
2026-01-24 12:19:46 -0300

    refactor: add MemberDeclarationBuilder

---
+++ Sources/SwiftStructure/Core/Visitors/Builders/MemberDeclarationBuilder.swift
@@ -0,0 +1,14 @@
+import SwiftSyntax
+
+struct MemberDeclarationBuilder: MemberOutputBuilder {
+    func build(from info: MemberDiscoveryInfo, using converter: SourceLocationConverter) -> MemberDeclaration {
+        let location = converter.location(for: info.position)
+        return MemberDeclaration(
+            name: info.name,
+            kind: info.kind,
+            line: location.line,
+            visibility: info.visibility,
+            isAnnotated: info.isAnnotated
+        )
+    }
+}
