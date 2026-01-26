3dd1fe5404d94e8bc94635692f93da79aaa89e24
Érico Perez Neto <ericodx@icloud.com>
2026-01-26 14:06:24 -0300

    style: add MARK comments to ReorderStage

--- Sources/SwiftStructure/Pipeline/Stages/Reorder/ReorderStage.swift
+++ Sources/SwiftStructure/Pipeline/Stages/Reorder/ReorderStage.swift
@@ -1,10 +1,16 @@
 struct ReorderStage: Stage {
 
+    // MARK: - Initialization
+
     init(configuration: Configuration = .default) {
         self.engine = ReorderEngine(configuration: configuration)
     }
 
+    // MARK: - Properties
+
     private let engine: ReorderEngine
+
+    // MARK: - Stage
 
     func process(_ input: ClassifyOutput) throws -> ReorderOutput {
         let results = input.declarations.map { decl in
