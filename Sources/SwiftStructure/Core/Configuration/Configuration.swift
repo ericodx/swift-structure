6d74b9451be3db8b62df137bf72f3f0674a9f8b6
Érico Perez Neto <ericodx@icloud.com>
2026-01-26 17:04:01 -0300

    style: add MARK comments to Configuration

--- Sources/SwiftStructure/Core/Configuration/Configuration.swift
+++ Sources/SwiftStructure/Core/Configuration/Configuration.swift
@@ -1,11 +1,15 @@
 struct Configuration: Equatable, Sendable {
 
+    // MARK: - Static Properties
+
     static let `default` = Configuration(
         version: 1,
         memberOrderingRules: MemberKind.allCases.map { .simple($0) },
         extensionsStrategy: .separate,
         respectBoundaries: true
     )
+
+    // MARK: - Properties
 
     let version: Int
     let memberOrderingRules: [MemberOrderingRule]
