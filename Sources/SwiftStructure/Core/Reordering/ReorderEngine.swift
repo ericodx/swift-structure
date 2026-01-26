b631b7c39a69ec8113a30fa3658bfd98a323630a
Érico Perez Neto <ericodx@icloud.com>
2026-01-26 14:06:21 -0300

    style: add MARK comments to ReorderEngine

--- Sources/SwiftStructure/Core/Reordering/ReorderEngine.swift
+++ Sources/SwiftStructure/Core/Reordering/ReorderEngine.swift
@@ -1,10 +1,16 @@
 struct ReorderEngine {
 
+    // MARK: - Initialization
+
     init(configuration: Configuration) {
         self.rules = configuration.memberOrderingRules
     }
 
+    // MARK: - Properties
+
     private let rules: [MemberOrderingRule]
+
+    // MARK: - Public Methods
 
     func reorder(_ members: [MemberDeclaration]) -> [MemberDeclaration] {
         members.sorted { lhs, rhs in
@@ -13,6 +19,8 @@ struct ReorderEngine {
             return lhsIndex < rhsIndex
         }
     }
+
+    // MARK: - Private Methods
 
     private func ruleIndex(for member: MemberDeclaration) -> Int {
         for (index, rule) in rules.enumerated() where rule.matches(member) {
