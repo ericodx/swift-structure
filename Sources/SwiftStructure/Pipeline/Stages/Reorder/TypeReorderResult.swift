1c3cda7bfce4957d45a23290cd0b4491d3e38226
Érico Perez Neto <ericodx@icloud.com>
2026-01-26 14:08:07 -0300

    style: add MARK comments to TypeReorderResult

--- Sources/SwiftStructure/Pipeline/Stages/Reorder/TypeReorderResult.swift
+++ Sources/SwiftStructure/Pipeline/Stages/Reorder/TypeReorderResult.swift
@@ -1,9 +1,14 @@
 struct TypeReorderResult: Sendable {
+
+    // MARK: - Properties
+
     let name: String
     let kind: TypeKind
     let line: Int
     let originalMembers: [MemberDeclaration]
     let reorderedMembers: [MemberDeclaration]
+
+    // MARK: - Computed Properties
 
     var needsReordering: Bool {
         originalMembers.map(\.kind) != reorderedMembers.map(\.kind)
