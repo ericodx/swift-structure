2c572013242f7ce3d9c9acc0369685d75d0a595d
Érico Perez Neto <ericodx@icloud.com>
2026-01-26 14:08:29 -0300

    style: add MARK comments to Stage

--- Sources/SwiftStructure/Pipeline/Stages/Protocols/Stage.swift
+++ Sources/SwiftStructure/Pipeline/Stages/Protocols/Stage.swift
@@ -1,9 +1,16 @@
 protocol Stage<Input, Output>: Sendable {
+
+    // MARK: - Associated Types
+
     associatedtype Input: Sendable
     associatedtype Output: Sendable
 
+    // MARK: - Methods
+
     func process(_ input: Input) throws -> Output
 }
+
+// MARK: - Stage + Composition
 
 extension Stage {
     func then<Next: Stage>(_ next: Next) -> Pipeline<Self, Next> where Output == Next.Input {
